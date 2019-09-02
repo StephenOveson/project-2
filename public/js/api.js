c27c78fa7cabea8670d00be728da4000ea706290
Spiritbr8ker <stephen.oveson@gmail.com>
2019-09-02 17:46:37 -0600

    service selectable and grabs id currently

---
+++ public/js/api.js
@@ -0,0 +1,32 @@
+var API = {
+  getServices: function() {
+    return $.ajax({
+      url: "api/services",
+      type: "GET"
+    });
+  }
+};
+
+// var selectService = function() {
+//   $("#selectService").on("click", function() {
+//     console.log($(this).data("id"));
+//   });
+// };
+
+var displayServices = function() {
+  API.getServices().then(function(data) {
+    var services = data.map(function(service) {
+      var button = $("<button>")
+        .text(service.name + " " + "$" + service.cost)
+        .attr("data-id", service.id)
+        .attr("id", "selectService")
+        .on("click", function() {
+          console.log($(this).data("id"));
+        });
+      return button;
+    });
+    $("#services").append(services);
+  });
+};
+
+displayServices();

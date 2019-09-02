375bfeadd71484c2a5fa827b4b3645bae49876a2
Spiritbr8ker <stephen.oveson@gmail.com>
2019-09-02 14:11:38 -0600

    fixed model for services.js file to exclude created at and updated at

--- models/services.js
+++ models/services.js
@@ -1,8 +1,14 @@
 module.exports = function(sequelize, DataTypes) {
-  var Services = sequelize.define("Services", {
-    name: DataTypes.STRING,
-    cost: DataTypes.DECIMAL(10, 2)
-  });
+  var Services = sequelize.define(
+    "Services",
+    {
+      name: DataTypes.STRING,
+      cost: DataTypes.DECIMAL(10, 2)
+    },
+    {
+      timestamps: false
+    }
+  );
   Services.associate = function(models) {
     models.Services.belongsToMany(models.Cosmetologists, {
       through: "CosmetologistService"

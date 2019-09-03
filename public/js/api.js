84a1ac4ae127a689690cc23cc5eb83408d16146d
Spiritbr8ker <stephen.oveson@gmail.com>
2019-09-03 00:59:32 -0600

    now calls and display cosmetologists

--- public/js/api.js
+++ public/js/api.js
@@ -21,13 +21,27 @@ var displayServices = function() {
         .attr("data-id", service.id)
         .addClass("service-button")
         .on("click", function() {
-          id = $(this).data("id");
+          $("#service-title").text("Cosmetologists");
+          var id = $(this).data("id");
           API.getCosmetologists(id).then(function(data) {
             var cosmetologists = data.map(function(cosmetologist) {
               var div = $("<div>");
-              var p = $("<p>").text(cosmetologist.Name);
+              var p = $("<p>")
+                .text("Would you like " + cosmetologist.Name)
+                .text("to perform " + cosmetologist.Services[0].name)
+                .text(
+                  "The " +
+                    cosmetologist.Services[0].name +
+                    " will cost " +
+                    "$" +
+                    cosmetologist.Services[0].cost
+                );
+              var select = $("<button>").text(cosmetologist.Name);
+              div.append(select);
               div.append(p);
+              return div;
             });
+            $("#services").empty();
             $("#services").append(cosmetologists);
           });
         });
